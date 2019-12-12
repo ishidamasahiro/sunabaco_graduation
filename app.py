6bfc1edcc90e830fd8d91bd786b4aa1a16caafc7
ishida <masahirori2015@gmail.com>
2019-12-12 15:00:02 +0900

    jsとcssを別ファイルにした

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import sqlite3
+import datetime
+from flask import Flask,render_template,request,redirect,session
+
 from flask import Flask
 app = Flask(__name__)
 
+# flaskでは標準でFlask.secret_keyを設定するとsessionを使うことができます
+app.secret_key = "sunabacoTakamatsu"
 
 
 @app.route('/')
 def hello_world():
-    return '<html><body><h1>sample</h1></body></html>'
+    return render_template("index.html")
 
 
 #おまじない--------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
