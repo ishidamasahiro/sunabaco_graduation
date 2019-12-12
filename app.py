f8a81f5218f6ece1f4100699c7fc3d2ccd25fa39
ishida <masahirori2015@gmail.com>
2019-12-12 15:53:46 +0900

    画面遷移を作った

--- app.py
+++ app.py
@@ -9,10 +9,30 @@ app = Flask(__name__)
 app.secret_key = "sunabacoTakamatsu"
 
 
-@app.route('/')
-def hello_world():
+#-----トップページ-----
+@app.route("/")
+def top():
     return render_template("index.html")
 
+#-----四国地図-----
+@app.route("/sikoku_map")
+def sikoku_map():
+    return render_template("sikoku_map.html")
+
+#-----香川地図-----
+@app.route("/kagawa_map")
+def kagawa_map():
+    return render_template("kagawa_map.html")
+
+#-----香川エリア01-----
+@app.route("/kagawa_arie01_map")
+def kagawa_arie01_map():
+    return render_template("kagawa_arie01_map.html")
+
+#-----寺基準のgoogleMapのページ-----
+@app.route("/googleMap_temple")
+def googleMap_temple():
+    return render_template("googleMap_temple.html")
 
 #おまじない--------------------------------------------
 if __name__ == "__main__":
